2a7f83f428735d13351114d16b83009a09f6a3fd
Albin Siriniqi <66482473+albinsiriniqi05@users.noreply.github.com>
2022-10-01 16:54:07 -0300

    Update __init__.py

--- kanban_website/__init__.py
+++ kanban_website/__init__.py
@@ -13,7 +13,8 @@ def create_app(test_config=None):
     app.config['SECRET_KEY'] = 'hjshjhdjah'
     #app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     #app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
-    app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://xjuggscflshuxc:5b607e1fa0f5912a337db154f7f8760e4dca301bcd2efc7fad9946388bcf577a@ec2-3-223-242-224.compute-1.amazonaws.com:5432/deav24ec3in2m9"
+    app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://xjuggscflshuxc:5b607e1fa0f5912a337db154f7f8760e4dca301bcd2efc7fad9946388bcf577a@ec2-3-223-242-224.compute-1.amazonaws.com:5432/deav24ec3in2m9
+"
 
     
     if test_config:
